766c0145f49927701bec7e1a8d1c3805bc2bb449
D7703_16 <d7703_16@D7703-16ui-iMac.local>
2018-05-03 11:22:39 +0900

    UIslider

--- UISlider Table/ViewController.swift
+++ UISlider Table/ViewController.swift
@@ -8,18 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
 
+    @IBOutlet weak var mySlider: UISlider!
+    @IBOutlet weak var myTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myTableView.dataSource = self
+        myTableView.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let identifier = "RE"
+        let cell = myTableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        
+        let tableValue = String(Int(mySlider.value * 100) + indexPath.row)
+        cell.textLabel?.text = tableValue
+        return cell
+    }
+    
+    @IBAction func sliderMoved(_ sender: Any) {
+        print("slider value = \(Int(mySlider.value * 100))")
+        
+        //tableView 값을 reload
+        myTableView.reloadData()
+    }
+    
 }
 
